ad0990d4d59273d31a42165771538f2b4cba7550
Yumu <945882678@qq.com>
2022-09-03 15:49:00 +0800

    set alias and css and proxy

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: error: git config user.name && git config user.email & please set dead value or install git
+ * @Date: 2022-09-03 13:29:16
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-09-03 15:35:01
+ * @FilePath: /my-vue-admin/src/App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
@@ -16,15 +24,21 @@ import HelloWorld from './components/HelloWorld.vue'
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
+<style lang="scss">
+#app {
+  background-color: $primaryColor;
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
+
+import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { wrapperEnv, createProxy } from './build/utils'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({ command,mode })=>{
+  const env = loadEnv(mode, process.cwd())
+  const viteEnv = wrapperEnv(env)
+
+  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY } = viteEnv
+  return {
+    plugins: [vue()],
+    base: VITE_PUBLIC_PATH || '/',
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname,'src')
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import '@/styles/variables.scss';`
+        }
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      port: VITE_PORT,
+      proxy: createProxy(VITE_PROXY)
+    }
+  }
 })
