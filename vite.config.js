4ea3cbec741b54370bb99ec7638ac4ff0e0fae2f
Yumu <945882678@qq.com>
2022-09-03 17:44:58 +0800

    集成插件

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-
-import { defineConfig,loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { wrapperEnv, createProxy } from './build/utils'
 import path from 'path'
+import { defineConfig,loadEnv } from 'vite'
+import { wrapperEnv, createProxy } from './build/utils'
+import { createVitePlugins } from './build/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command,mode })=>{
+  const isBuild = command === 'build'
   const env = loadEnv(mode, process.cwd())
   const viteEnv = wrapperEnv(env)
-
   const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY } = viteEnv
+  
   return {
-    plugins: [vue()],
+    plugins: createVitePlugins(viteEnv, isBuild),
     base: VITE_PUBLIC_PATH || '/',
     resolve: {
       alias: {
